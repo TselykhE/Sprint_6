f3544f7fcde20288440bc9f88296b6632ae2b439
Evgeniia Tselykh <tea2355@yandex.ru>
2025-04-03 17:43:40 +0300

    Исправлено: Можно лучше: урлы стоит конкатенировать с базовым, так будет проще поддерживать несколько стендов.

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 class Urls:
-    MAIN_PAGE_URL = 'https://qa-scooter.praktikum-services.ru/'
-    ORDER_PAGE_URL = 'https://qa-scooter.praktikum-services.ru/order'
+    MAIN_PAGE_URL = 'https://qa-scooter.praktikum-services.ru'
+    ORDER_PAGE_URL = f"{MAIN_PAGE_URL}/order"
     DZEN_URL = 'https://dzen.ru/?yredirect=true'
 
 
