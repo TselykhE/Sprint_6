d016ba8cf607b5441302f20d8d5e067fa3a96858
Evgeniia Tselykh <tea2355@yandex.ru>
2025-06-04 12:51:52 +0300

    Удалён лишний коммент

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -4,8 +4,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
 
     ORDER_BUTTON_HEADER = By.XPATH, "//button[text()='Заказать']"
-    ORDER_CENTER_BUTTON = By.CLASS_NAME, "Home_FinishButton__1_cWm"
-        # By.XPATH, "//div[contains(@class, 'Home_FinishButton__1_cWm')]//button")
+    ORDER_CENTER_BUTTON = By.XPATH, "//div[contains(@class, 'Home_FinishButton__1_cWm')]//button"
 
     QUESTION = By.XPATH, "//div[@id='accordion__heading-{}']"
     ANSWER = By.XPATH, "//div[@id='accordion__panel-{}']"
